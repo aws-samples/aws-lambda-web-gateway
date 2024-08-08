d79b9468cf53e147efa98ec5b7c57b19cca308de
Harold Sun <sunhua@amazon.com>
2024-08-08 14:18:50 +0000

    refactor: remove command line argument parsing from main.rs

--- src/main.rs
+++ src/main.rs
@@ -35,57 +35,6 @@ use tokio_stream::wrappers::ReceiverStream;
 use tower_http::trace::TraceLayer;
 use tracing::info;
 
-    let matches = Command::new("lambda-gateway")
-        .version("0.1.0")
-        .author("Harold Sun <sunhua@amazon.com>")
-        .about("A gateway to AWS Lambda functions")
-        .arg(
-            Arg::new("lambda-function-name")
-                .short('f')
-                .long("lambda-function-name")
-                .value_name("FUNCTION_NAME")
-                .help("Sets the Lambda function name")
-                .required(true)
-                .value_parser(value_parser!(String)),
-        )
-        .arg(
-            Arg::new("lambda-invoke-mode")
-                .short('m')
-                .long("lambda-invoke-mode")
-                .value_name("INVOKE_MODE")
-                .help("Sets the Lambda invoke mode")
-                .required(true)
-                .value_parser(["Buffered", "ResponseStream"]),
-        )
-        .arg(
-            Arg::new("api-keys")
-                .short('k')
-                .long("api-keys")
-                .value_name("API_KEYS")
-                .help("Sets the API keys")
-                .required(true)
-                .value_parser(value_parser!(String)),
-        )
-        .get_matches();
-
-    let lambda_function_name = matches.get_one::<String>("lambda-function-name").unwrap().clone();
-    let lambda_invoke_mode = match matches.get_one::<String>("lambda-invoke-mode").unwrap().as_str() {
-        "Buffered" => LambdaInvokeMode::Buffered,
-        "ResponseStream" => LambdaInvokeMode::ResponseStream,
-        _ => return Err("Invalid invoke mode".into()),
-    };
-    let api_keys: HashSet<String> = matches.get_many::<String>("api-keys")
-        .unwrap()
-        .map(|s| s.clone())
-        .collect();
-
-    Ok(Config {
-        lambda_function_name,
-        lambda_invoke_mode,
-        api_keys,
-    })
-}
-
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
